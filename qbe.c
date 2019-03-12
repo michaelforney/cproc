4a17a825e5944041bbcd4aed6a85fb7590247a62
Michael Forney <mforney@mforney.org>
2019-03-12 00:41:54 -0700

    Revert "Zero uninitialized decls/returns."
    This reverts commit 7722c7593a1a249e1a6d98d84afdff3cb086ca81.
    
    This causes problems with large stack allocations, such as
    https://github.com/madler/pigz/blob/fe822cb435622c43f491013da77b127e9fe851a9/pigz.c#L3403-L3405

--- qbe.c
+++ qbe.c
@@ -422,13 +422,6 @@ funcjnz(struct function *f, struct value *v, struct value *l1, struct value *l2)
 void
 funcret(struct function *f, struct value *v)
 {
-	struct declaration *d;
-
-	if (!v && f->type->base->kind != TYPEVOID) {
-		d = mkdecl(DECLOBJECT, f->type->base, LINKNONE);
-		funcinit(f, d, NULL);
-		v = funcload(f, d->type, d->value);
-	}
 	funcinst(f, IRET, NULL, v);
 	f->end->terminated = true;
 }
@@ -891,6 +884,8 @@ funcinit(struct function *func, struct declaration *d, struct initializer *init)
 	size_t i;
 
 	funcalloc(func, d);
+	if (!init)
+		return;
 	for (; init; init = init->next) {
 		zero(func, d->value, d->type->align, offset, init->start);
 		if (init->expr->kind == EXPRSTRING) {
@@ -1101,7 +1096,7 @@ emitfunc(struct function *f, bool global)
 	size_t n;
 
 	if (!f->end->terminated)
-		funcret(f, NULL);
+		funcret(f, strcmp(f->name, "main") == 0 ? mkintconst(&i32, 0) : NULL);
 	if (global)
 		puts("export");
 	fputs("function ", stdout);
