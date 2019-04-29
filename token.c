2ed7304347656421be2bc0140df18c618454bf72
Michael Forney <mforney@mforney.org>
2019-04-29 00:13:59 -0700

    Add __typeof__ to tokstr

--- token.c
+++ token.c
@@ -55,6 +55,7 @@ const char *tokstr[] = {
 	[T_NORETURN] = "_Noreturn",
 	[T_STATIC_ASSERT] = "_Static_assert",
 	[T_THREAD_LOCAL] = "_Thread_local",
+	[T__TYPEOF__] = "__typeof__",
 
 	/* punctuator */
 	[TLBRACK] = "[",
